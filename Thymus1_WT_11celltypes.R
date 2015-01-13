6189b3986ef7e2d238287b220405109d0d6ccbde
mrougeau7 <mrougeau7@gmail.com>
2015-01-12 20:52:35 -0500

    thesis changes

---
+++ Thymus1_WT_11celltypes.R
@@ -0,0 +1,298 @@
+setwd("~/GitHub/Thymus/Datasets/Data with 11 cell types/Extra/Reprocessing_WT1/txt_2")
+
+library(gplots)
+
+mszs=100  #matrix size in hist2d
+clusters=4  #number of clusters produced for K means
+txtfiless=list.files(pattern="*.txt")  #loads in order all files within folder
+
+mats<-matrix(data=NA,nrow=10000,ncol=33) #this will change with size of matrix and # of sections/cell types
+
+toFills<-as.data.frame(mats)  #changes to data frame
+
+toUse0s<-seq(1,33,1) #uses all 33 columns
+toUse1s<-seq(1,33,3) #uses columns that correspond to Section1
+toUse2s<-seq(2,33,3) #Section2
+toUse3s<-seq(3,33,3) #Section3
+
+for(i in toUse0s){
+  tmps = read.table(txtfiless[i], sep="\t", head=T)
+  Xs=tmps[[1]]  #[,1]
+  Ys=tmps[[2]]
+  my.xys<-hist2d(Xs,Ys,nbins=c(mszs,mszs))
+  objs <- my.xys$counts
+  
+  objs[1,1] = objs[1,1]-1
+  objs[100,1] = objs[100,1]-1
+  objs[1,100] = objs[1,100]-1
+  objs[100,100]=objs[100,100]-1
+  
+  counts<- as.vector(objs)
+  toFills[,i]<-counts
+  
+  results<-toFills[,toUse0s]
+  results->result0s
+}
+
+for(i in toUse1s){
+  tmps = read.table(txtfiless[i], sep="\t", head=T)
+  Xs=tmps[[1]]  #[,1]
+  Ys=tmps[[2]]
+  my.xys<-hist2d(Xs,Ys,nbins=c(mszs,mszs))
+  objs <- my.xys$counts
+  
+  objs[1,1] = objs[1,1]-1
+  objs[100,1] = objs[100,1]-1
+  objs[1,100] = objs[1,100]-1
+  objs[100,100]=objs[100,100]-1
+  
+  counts<- as.vector(objs)
+  toFills[,i]<-counts
+  
+  results<-toFills[,toUse1s]
+  results->result1s
+}
+
+for(i in toUse2s){
+  tmps = read.table(txtfiless[i], sep="\t", head=T)
+  Xs=tmps[[1]]  #[,1]
+  Ys=tmps[[2]]
+  my.xys<-hist2d(Xs,Ys,nbins=c(mszs,mszs))
+  objs <- my.xys$counts
+  
+  objs[1,1] = objs[1,1]-1
+  objs[100,1] = objs[100,1]-1
+  objs[1,100] = objs[1,100]-1
+  objs[100,100]=objs[100,100]-1
+  
+  counts<- as.vector(objs)
+  toFills[,i]<-counts
+  
+  results<-toFills[,toUse2s]
+  results->result2s
+}
+
+for(i in toUse3s){
+  tmps = read.table(txtfiless[i], sep="\t", head=T)
+  Xs=tmps[[1]]  #[,1]
+  Ys=tmps[[2]]
+  my.xys<-hist2d(Xs,Ys,nbins=c(mszs,mszs))
+  objs <- my.xys$counts
+  
+  objs[1,1] = objs[1,1]-1
+  objs[100,1] = objs[100,1]-1
+  objs[1,100] = objs[1,100]-1
+  objs[100,100]=objs[100,100]-1
+  
+  counts<- as.vector(objs)
+  toFills[,i]<-counts
+  
+  results<-toFill[,toUse3s]
+  results->result3s   ###Section3
+}
+
+
+library (vegan)
+library(labdsv)
+
+# This step normalizes your data and is optional.
+#spe.std <- decostand(spe, "normalize")  #You can also use "standardize". See 'help' for details.
+
+# Do the k-means clustering [read 'help' for the 'kmeans' function to see what the arguments "centers" (clusters or k) and "nstart" (randomizations) mean].
+
+spe.kmeans_Mutant1_Alls <- kmeans(result0s, centers=clusters, nstart=10)
+spe.kmeans_Mutant1_S1s <- kmeans(result1s, centers=clusters, nstart=10)
+spe.kmeans_Mutant1_S2s <- kmeans(result2s, centers=cluster, nstart=10)
+spe.kmeans_Mutant1_S3s <- kmeans(result3s, centers=cluster, nstart=10)
+
+library(gplots) ###Call 1 set at a time; produces image of section
+
+v1s <- spe.kmeans_Mutant1_S1s$cluster  #change this to section you are interested in looking at
+
+#load("thymus.Rdata")
+#image(v2) # make pic
+v2s = matrix(v1s, nrow = 100, ncol=100)
+v2s<-v2s[1:40,1:100] # trim
+image(v2s) # make new pic
+
+heatmap.2( v2s, Rowv=FALSE, Colv=FALSE, dendrogram='none', cellnote=v2s,
+notecol="black", trace='none', key=FALSE,lwid = c(.01,0.99),lhei = c(.01,.99),
+margins = c(0,0),col=c("green", "yellow", "blue", "red")) 
+
+clust.exts<-v2s[1,1] # get cluster number of corner pixel (should always represent 'exterior')
+idxs<-which(v2s==clust.exts,arr.ind=T) # get addresses of all pixels of that number
+#mat[mat < 0.1] <- NA
+idx2s<-which(idxs[,1]<40) # find such pixels for which there is another pixel immediately to right
+idxs<-idxs[idx2s,] # use only those pixels
+idxs[,1]<-idxs[,1]+1 # find pixels immediately to right
+zs<-table(v2s[idxs]) # make sorted table of cluster numbers of these pixels
+clust.ints<-as.integer(which(zs==max(zs[zs!=max(zs)]))) # get the cluster number that is 2nd most common (this is the thing most often 'touching' exterior, i.e. interior but not medular)
+#mat[mat < 0.1] <- NA
+
+clust.med1s<-as.integer(which(zs==min(zs[zs!=min(zs)])))
+clust.med2s<-as.integer(which(zs==min(zs)))
+idx.med1s<-which(v2s==clust.med1s)
+idx.med2s<-which(v2s==clust.med2s)
+
+####ADD s
+
+mean(toFill[idx.med1,1],na.rm=T) #looking at certain column numbers
+mean(toFill[idx.med2,1],na.rm=T)
+mean(toFill[idx.med1,22],na.rm=T)
+mean(toFill[idx.med2,22],na.rm=T)
+mean(toFill[idx.med1,25],na.rm=T)
+mean(toFill[idx.med2,25],na.rm=T)
+
+col1.01<-1*(toFill[,1]>0)
+col22.01<-1*(toFill[,22]>0)
+col25.01<-1*(toFill[,25]>0)
+
+mean(col1.01[idx.med1],na.rm=T)
+mean(col1.01[idx.med2],na.rm=T)
+mean(col22.01[idx.med1],na.rm=T)
+mean(col22.01[idx.med2],na.rm=T)
+mean(col25.01[idx.med1],na.rm=T)
+mean(col25.01[idx.med2],na.rm=T)
+
+
+
+heatmap.2( v2, Rowv=FALSE, Colv=FALSE, dendrogram='none', cellnote=v2,
+notecol="black", trace='none', key=FALSE,lwid = c(.01,0.99),lhei = c(.01,.99),
+margins = c(0,0),col=c("green", "yellow", "blue", "red")) 
+
+
+my.order<-c(1,2,3,4) # define the order we want to plot panels
+par(mfrow=c(2,2)) # make 4 subplots in 2x2 style
+for (i in 1:max(spe.kmeans_Mutant1_All$cluster)){ 
+barplot(colSums(result0[which(spe.kmeans_Mutant1_All1$cluster==i),]),main=i,ylim=c(0,12000)) 
+# we pick out desired cluster and plot
+}
+
+#my.order<-c(1,2,3,4) # define the order we want to plot panels
+#par(mfrow=c(2,2)) # make 4 subplots in 2x2 style
+#for (i in 1:max(spe.kmeans_Mutant1_S1$cluster)){ 
+#  barplot(colSums(result1[which(spe.kmeans_Mutant1_S1$cluster==i),]),main=i,ylim=c(0,12000)) 
+# we pick out desired cluster and plot
+#}
+
+#my.order<-c(1,2,3,4) # define the order we want to plot panels
+#par(mfrow=c(2,2)) # make 4 subplots in 2x2 style
+#for (i in 1:max(spe.kmeans_Mutant1_S2$cluster)){ 
+#  barplot(colSums(result2[which(spe.kmeans_Mutant1_S2$cluster==i),]),main=i,ylim=c(0,12000)) 
+# we pick out desired cluster and plot
+#}
+
+#my.order<-c(1,2,3,4) # define the order we want to plot panels
+#par(mfrow=c(2,2)) # make 4 subplots in 2x2 style
+#for (i in 1:max(spe.kmeans_Mutant1_S3$cluster)){ 
+#  barplot(colSums(result3[which(spe.kmeans_Mutant1_S3$cluster==i),]),main=i,ylim=c(0,12000)) 
+# we pick out desired cluster and plot
+#}
+
+b2<-colSums(result0[which(spe.kmeans_Mutant1_All$cluster==2),])
+b3<-colSums(result0[which(spe.kmeans_Mutant1_All$cluster==3),])
+b4<-colSums(result0[which(spe.kmeans_Mutant1_All$cluster==4),])
+
+#b2_S1<-colSums(result1[which(spe.kmeans_Mutant1_S1$cluster==2),])
+#b3_S1<-colSums(result1[which(spe.kmeans_Mutant1_S1$cluster==3),])
+#b4_S1<-colSums(result1[which(spe.kmeans_Mutant1_S1$cluster==4),])
+
+#b2_S2<-colSums(result2[which(spe.kmeans_Mutant1_S2$cluster==2),])
+#b3_S2<-colSums(result2[which(spe.kmeans_Mutant1_S2$cluster==3),])
+#b4_S2<-colSums(result2[which(spe.kmeans_Mutant1_S2$cluster==4),])
+
+#b2_S3<-colSums(result3[which(spe.kmeans_Mutant1_S3$cluster==2),])
+#b3_S3<-colSums(result3[which(spe.kmeans_Mutant1_S3$cluster==3),])
+#b4_S3<-colSums(result3[which(spe.kmeans_Mutant1_S3$cluster==4),])
+
+###and normalize these so that each cluster type has the same total amount of cells (in other words, we're ###getting the proportion of cell types in each cluster)
+
+
+b2<-b2/sum(b2)
+b3<-b3/sum(b3)
+b4<-b4/sum(b4)
+
+#b2_S1<-b2/sum(b2_S1)
+#b3_S1<-b3/sum(b3_S1)
+#b4_S1<-b4/sum(b4_S1)
+
+#b2_S2<-b2/sum(b2_S2)
+#b3_S2<-b3/sum(b3_S2)
+#b4_S2<-b4/sum(b4_S2)
+
+#b2_S3<-b2/sum(b2_S3)
+#b3_S3<-b3/sum(b3_S3)
+#b4_S3<-b4/sum(b4_S3)
+
+#We can look at the difference between pairs of clusters to see which cell types are most different between #clusters
+
+par(mfrow=c(2,2))  ##All
+barplot(b2-b3,main="2-3")
+barplot(b2-b4,main="2-4")
+barplot(b3-b4,main="3-4")
+
+#par(mfrow=c(2,2))
+#barplot(b2-b3,main="2-3")
+#barplot(b2-b4,main="2-4")
+#barplot(b3-b4,main="3-4")
+
+#par(mfrow=c(2,2))
+#barplot(b2-b3,main="2-3")
+#barplot(b2-b4,main="2-4")
+#barplot(b3-b4,main="3-4")
+
+#par(mfrow=c(2,2))
+#barplot(b2-b3,main="2-3")
+#barplot(b2-b4,main="2-4")
+#barplot(b3-b4,main="3-4")
+
+###Bray Curtis
+
+library(vegan) 
+library(dplyr) # used for chaining and manipulation  
+# http://blog.rstudio.org/2014/01/17/introducing-dplyr/
+#reads as: take only cluster 1 data from 'spe', then take column sums (i.e. total cell type count) 
+#then transpose (swap rows/cols needed for bray curtis calc)
+
+df<-result0[which(spe.kmeans_Mutant1_All$cluster==1),] %>% colSums() %>% t()   # example of chaining. 
+for (j in 2:4){  
+  tmp<-result0[which(spe.kmeans_Mutant1_All$cluster==j),] %>% colSums() %>% t() 
+  df<-rbind(df,tmp)
+}
+
+#df1.1<-spe1.1[which(spe.kmeans1.1$cluster==1),] %>% colSums() %>% t()
+#for (j in 2:4){  
+#  tmp1.1<-spe1.1[which(spe.kmeans1.1$cluster==j),] %>% colSums() %>% t() 
+#  df1.1<-rbind(df1.1,tmp1.1)
+#}
+
+#df1.2<-spe1.2[which(spe.kmeans1.2$cluster==1),] %>% colSums() %>% t()
+#for (j in 2:4){  
+#  tmp1.2<-spe1.2[which(spe.kmeans1.2$cluster==j),] %>% colSums() %>% t() 
+#  df1.2<-rbind(df1.2,tmp1.2)
+#}
+
+#df1.3<-spe1.3[which(spe.kmeans1.3$cluster==1),] %>% colSums() %>% t()
+#for (j in 2:4){  
+#  tmp1.2<-spe1.3[which(spe.kmeans1.3$cluster==j),] %>% colSums() %>% t() 
+#  df1.3<-rbind(df1.3,tmp1.3)
+#}
+
+
+df<-apply(df,2,as.integer) %>% as.data.frame()
+#df1.1<-apply(df1.1,2,as.integer) %>% as.data.frame()
+#df1.2<-apply(df1.2,2,as.integer) %>% as.data.frame()
+#df1.3<-apply(df1.3,2,as.integer) %>% as.data.frame()
+
+#need same number of columns to bind them.
+colnames(df2)<-colnames(df)
+#colnames(df1.1)=colnames(df2.1)
+#colnames(df1.2)<-colnames(df2.1)
+#colnames(df1.2)<-colnames(df2.1)
+
+#dfTOTAL<-rbind(df1.2,df2.2, df1.2, df1.2, df1.1, df1.1)
+
+BrayCurtis<-vegdist(dfTOTAL,method="bray")
+#print(BrayCurtis)
+hc<-hclust(BrayCurtis)
+plot(hc,labels=dfTOTAL$rownames)
